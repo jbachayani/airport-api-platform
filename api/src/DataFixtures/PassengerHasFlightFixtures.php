4ff7523e9311a49e861be2a2caf00ae2340e78a3
Sinicha <sinirados@gmail.com>
2019-03-08 09:11:10 +0100

    Add PassengerHasFlight Fixture

---
+++ api/src/DataFixtures/PassengerHasFlightFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Airport;
+use App\Entity\Passenger;
+use App\Entity\Seat;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class PassengerHasFlightFixtures extends Fixture implements DependentFixtureInterface
+{
+    const QT = 800;
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $passengers = $manager->getRepository(Passenger::class)->findAll();
+        $airports = $manager->getRepository(Airport::class)->findAll();
+        $seats = $manager->getRepository(Seat::class)->findAll();
+
+        for ($i = 0; $i < self::QT; $i++) {
+            $pasHasFlight = new PassengerHasFlight();
+            $manager->persist($pasHasFlight);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            AirportFixtures::class,
+            SeatFixtures::class,
+            PassengerFixtures::class
+        ];
+    }
+}
